187a05c71737010d1ae8020dc991f40246965deb
Sean <spryan27@gmail.com>
2020-05-04 20:59:40 -0500

    Created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+   Blog.create!(
+     title: "My blog post #{blog}",
+     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+   )
+end
+
+puts "Blogs created"
+
+5.times do |skill|
+	Skill.create!(
+		title: "Rails #{skill}",
+		precent_utilized: 15
+    )
+end
+
+puts "Skills created"
+
+9.times do |portfolia_item|
+  Portfolium.create!(
+     title: "Portfolio title: #{portfolia_item}",
+     subtitle: "Dope ass shit",
+     body: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+     main_image: "https://via.placeholder.com/600x400",
+     thumb_image: "https://via.placeholder.com/350x200"
+  	)
+end
