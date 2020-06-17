540be2ebd4e62ee139fdb033fb02764f11c51b53
Sean <spryan27@gmail.com>
2020-06-16 21:12:20 -0500

    added new and create actions for portfolios

--- app/controllers/portfolia_controller.rb
+++ app/controllers/portfolia_controller.rb
@@ -2,4 +2,22 @@ class PortfoliaController < ApplicationController
   def index
   	@portfolio_items = Portfolium.all
   end
+
+  def new
+  	@portfolio_item = Portfolium.new
+  end
+
+  def create
+    @portfolio_item = Portfolium.new(params.require(:portfolium).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolia_path, notice: 'Portfolio item was successfully created.' }
+        format.json { render :show, status: :created, location: @portfolio_item }
+      else
+        format.html { render :new }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
